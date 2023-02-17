326f5021f2953e9c3017e448895200355c03c0ca
TobiLight <jgetitdonefast@gmail.com>
2023-02-16 17:51:53 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
-
 /**
- * main - Entry Point
- *
- * Description: Prints "Programming is like building a multilingual puzzle"
- *
- * Return: 0 (Success) 
- */
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
