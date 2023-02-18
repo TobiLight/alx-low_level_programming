b4afdd037c443851e9b6d5f11287fc0b22907a21
TobiLight <jgetitdonefast@gmail.com>
2023-02-17 16:14:04 -0800

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints the lowercase alphabet in reverse
+ *
+ * Description: prints the lowercase alphabet in reverse
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i;
+
+	i = 123;
+
+	while (i > 96)
+	{
+		putchar(i);
+		i--;
+	}
+	putchar(10);
+
+	return (0);
+}
