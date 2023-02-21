594a579f3e5dbb753f8cdea68f4684b663d8760c
TobiLight <jgetitdonefast@gmail.com>
2023-02-21 15:49:45 -0800

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - prints _putchar, followed by a new line
+ *
+ * Description: prints _putchar, followed by a new line
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char text[] = "_putchar";
+	int i = 0;
+
+	for (i = 0; i < 0; i++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
