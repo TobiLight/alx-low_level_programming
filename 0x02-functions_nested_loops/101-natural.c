94ff2014a7f5d2e1856ced4396715ee214c4e369
TobiLight <jgetitdonefast@gmail.com>
2023-02-21 19:12:58 -0800

    Nature made the natural numbers; All else is the work of women

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - list all natural numbers multiples of 3 & 5
+ *
+ * Return: Alaways 0 (Success)
+*/
+
+int main(void)
+{
+	int num, sum;
+
+	sum = 0;
+
+	for (num = 5; num < 1024; num++)
+	{
+		if (num % 3 == 0 || num % 5 == 0)
+		{
+			sum += num;
+		}
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
