d7e9953f69d3cf4407dbedcfb3bf0be4c3f27c67
TobiLight <jgetitdonefast@gmail.com>
2023-02-23 15:47:45 -0800

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,15 +3,13 @@
  * _isupper - checks for uppercase character
  * @c: c is an integer
  *
- * Return: 1 if uppercase
+ * Return: 1 if uppercase, 0 if not uppercase
 */
 
 int _isupper(int c)
 {
 	if (c >= 'A' && C <= 'Z')
-	{
 		return (1);
-	}
 
 	return (0);
 }
