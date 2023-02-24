0f9e87ef6f7eed14b0963c9487b9ca4c18e97a75
TobiLight <jgetitdonefast@gmail.com>
2023-02-23 16:04:03 -0800

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9, followed by a new line.
+ *
+ * Return: void
+*/
+
+void print_numbers(void)
+{
+	char c;
+
+	_putchar(c);
+}
