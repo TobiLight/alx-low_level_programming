0de2340b042a2b0d736de8ed61e2acdaca2bff0e
TobiLight <jgetitdonefast@gmail.com>
2023-02-27 16:07:48 -0800

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "mainh"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string parameter
+ *
+ * Return: Integer
+*/
+
+int _strlen(char *s)
+{
+	int counter;
+
+	for (counter = 0; *s != '\0'; s++)
+		counter++;
+
+	return (counter);
+}
