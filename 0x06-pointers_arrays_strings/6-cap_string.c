3acdc1342f88180144ef3d63f2a7c92325235f1d
TobiLight <jgetitdonefast@gmail.com>
2023-03-02 15:03:41 -0800

    Expect the best. Prepare for the worst. Capitalize on what comes

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string.
+ * @s: s is a character
+ *
+ * Return: Character
+*/
+
+char *cap_string(char *s)
+{
+	int i, j;
+	char separators[] = " \t\n,;.!?\"{}()";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			if (i == 0)
+			{
+				s[i] -= 32;
+			}
+			else
+			{
+				for (j = 0; separators[j] != '\0'; j++)
+				{
+					if (separators[j] == s[i - 1])
+							s[i] -= 32;
+				}
+			}
+		}
+	}
+	return (s);
+}
