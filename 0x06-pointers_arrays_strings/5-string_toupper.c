96971c35a4ff078d107c119c081d7d00d57b913e
TobiLight <jgetitdonefast@gmail.com>
2023-03-02 14:35:02 -0800

    Always look up

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,1 +1,22 @@
+#include "main.h"
 
+/**
+ * string_toupper - changes all lowercase letters
+ * of a string to uppercase
+ * @s: s is a string
+ *
+ * Return: Character
+*/
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
+	}
+
+	return (s);
+}
