349a21e8816dae7f3a58ecc6b9f85e4c6d64b185
TobiLight <jgetitdonefast@gmail.com>
2023-03-02 12:52:41 -0800

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,16 +14,20 @@ char *_strncpy(char *dest, char *src, int n)
 	char *result = dest;
 	int i;
 
-	/* Loop through n and copy src[i] to dest[i]
-	 * if src[i] doesnt have null byte */
+	/**
+	 * Loop through n and copy src[i] to dest[i]
+	 * * if src[i] doesnt have null byte
+	*/
 	for (i = 0; i < n && src[i] != '\0')
 	{
 		/* Copy src[i] to dest */
 		dest[i] = src[i];
 	}
 
-	/* Loop through n again so dest can have
-	 * all it's remaining characters */
+	/**
+	 * Loop through n again so dest can have
+	 * all it's remaining characters
+	*/
 	for (; i < n; i++)
 	{
 		dest[i] = '\0';
