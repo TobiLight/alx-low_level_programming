c5f8a08c8341f62991df6923ab4fd18a0e0fe09e
TobiLight <jgetitdonefast@gmail.com>
2023-03-01 19:07:07 -0800

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -6,31 +6,29 @@
  * @src: src is a character appended to dest
  * @n: n is an integer
  *
- * Description:
+ * Return: Pointer
  *
 */
 
 char *_strncat(char *dest, char *src, int n)
 {
-    char *result = dest;
 
+	char *result = dest;
 
-    /* get dest pointer to the end of the string*/
-    while (*dest != '\0')
-    {
-        dest++;
-    }
+	while (*dest != '\0')
+	{
+		 dest++;
+	}
 
+	while (*src != '\0' && n > 0)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		n--;
+	}
 
-    while (*src != '\0' && n > 0)
-    {
-        *dest = *src;
-        dest++;
-        src++;
-        n--;
-    }
+	*dest = '\0';
 
-    *dest = '\0';
-
-    return (result);
+	return (result);
 }
