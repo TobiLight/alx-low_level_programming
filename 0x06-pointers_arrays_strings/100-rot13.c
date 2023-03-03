b54e5c84926916d0c8d389becab7cfd860fd48e1
TobiLight <jgetitdonefast@gmail.com>
2023-03-02 16:40:37 -0800

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string into rot13
+ * @s: s is a character
+ *
+ * Return: Character
+*/
+
+char *rot13(char *s)
+{
+	char words[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char words_13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; words[j] != '\0'; j++)
+		{
+			if (s[i] == words[j])
+			{
+				s[i] = words_13[j];
+			}
+		}
+	}
+
+	return (s);
+}
