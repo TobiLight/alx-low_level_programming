929c188da9c93461d2360a5c8f358fcc1e2d92f0
TobiLight <jgetitdonefast@gmail.com>
2023-03-08 15:08:26 -0800

    Once an idea has taken hold of the brain it's almost impossible to eradicate

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,30 @@
+/*
+ * File: 4-pow_recursion
+ * Author: TobiLight
+*/
+
+#include "main.h"
+
+/**
+ * _pow_recursion -  returns the value of x raised to the power of y.
+ * @x: x is an integer
+ * @y: y is an integer
+ *
+ * Return: If y < 0 - -1 to indicate an error
+ * If y > 0 - pow of x raised to the power of y
+*/
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+
+	if (y == 0)
+	{
+		return (1);
+	}
+
+	return (x * _pow_recursion(x, y - 1));
+}
