541e150f5d5051b162df3f5d98bcaf9fe009f76d
TobiLight <jgetitdonefast@gmail.com>
2023-03-13 17:37:00 -0700

    The woman who has no imagination has no wings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,38 @@
+/*
+ * File: 1-strdup.c
+ * Author: TobiLight
+*/
+
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup -  returns a pointer to a newly allocated
+ *            space in memory
+ * @str: string to be duplicated
+ *
+ * Return: Pointer (char) or NULL
+*/
+
+char *_strdup(char *str)
+{
+	size_t str_len;
+	char *new_str;
+
+	/* Return NULL if str = NULL */
+	if (str == NULL)
+		return (NULL);
+
+	/* Get length of str */
+	str_len = strlen(str);
+
+	/* Allocate memory for new_str */
+	new_str = malloc(str_len + 1);
+
+	/* copy str int new_str */
+	memcpy(new_str, str);
+
+	return new_str;
+}
