c3a39451923132c99f3f25210074bcc94a958e34
TobiLight <jgetitdonefast@gmail.com>
2023-03-16 15:17:49 -0700

    _calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,41 @@
+/*
+ * File: 2-calloc.c
+ * Author: TobiLight
+*/
+
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory for an array
+ * @nmemb: Number of elements
+ * @size: Byte size of each array element
+ *
+ * Return: If nmemb = 0, size = 0, or the function fails - NULL.
+ *         Otherwise - a pointer to the allocated memory.
+*/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+	unsigned int total_elem, i;
+	char *char_ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	total_elem = nmemb * size;
+	ptr = malloc(total_elem);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	/* Initialize memory to 0 */
+	char_ptr = ptr;
+	for (i = 0; i < total_elem; i++)
+	{
+		*(char_ptr + 1) = 0;
+	}
+
+	return (ptr);
+}
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -13,3 +13,10 @@ This projecy is focused on the use of malloc and free.
   * If the function fails, it should return `NULL`
   * If `n` is greater or equal to the length of `s2` then use the entire string `s2`
   * if `NULL` is passed, treat it as an empty string
+
+* **[2-calloc.c](./2-calloc.c)** - Write a function that allocates memory for an array, using malloc.
+	* Prototype: `void *_calloc(unsigned int nmemb, unsigned int size);`
+	* The `_calloc` function allocates memory for an array of `nmemb` elements of `size` bytes each and returns a pointer to the allocated memory.
+	* The memory is set to zero
+	* If `nmemb` or `size` is `0`, then `_calloc` returns `NULL`
+	* If `malloc` fails, then `_calloc` returns `NULL`
