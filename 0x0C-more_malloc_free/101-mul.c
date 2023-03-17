ffbe1f3249e2c6a734c653c4a5cada11f60b77bb
Agunloye Oluwatobiloba <jgetitdonefast@gmail.com>
2023-03-17 02:40:50 +0100

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -54,54 +54,62 @@ void print_result(char *result, int len)
 
 char *multiply(char *num1, char *num2)
 {
-    int len1 = 0, len2 = 0, i, j, carry, sum;
-    char *result;
+	int len1 = 0, len2 = 0, i, j, carry, sum;
+	char *result;
 
-    /* Compute the length of the input strings */
-    while (num1[len1])
-        len1++;
-    while (num2[len2])
-        len2++;
+	/* Compute the length of the input strings */
+	while (num1[len1])
+		len1++;
+	while (num2[len2])
+		len2++;
 
-    /* Allocate space for the result string */
-    result = malloc(len1 + len2 + 1);
-    if (!result)
-        return (NULL);
+	/* Allocate space for the result string */
+	result = malloc(len1 + len2 + 1);
+	if (!result)
+		return (NULL);
 
-    /* Initialize the result string to zero */
-    for (i = 0; i < len1 + len2; i++)
-        result[i] = '0';
-    result[i] = '\0';
+	/* Initialize the result string to zero */
+	for (i = 0; i < len1 + len2; i++)
+	{
+		result[i] = '0';
+		result[i] = '\0';
+	}
+	
+	/* Multiply each digit of num1 with each digit of num2 */
+	for (i = len1 - 1; i >= 0; i--)
+	{
+		if (num1[i] < '0' || num1[i] > '9')
+		    return (NULL);
+		carry = 0;
+		for (j = len2 - 1; j >= 0; j--)
+		{
+			if (num2[j] < '0' || num2[j] > '9')
+				return (NULL);
+			sum = (num1[i] - '0') * (num2[j] - '0') + carry + (result[i + j + 1] - '0');
+			carry = sum / 10;
+			result[i + j + 1] = (sum % 10) + '0';
+		}
+		result[i + j + 1] = carry + '0';
+	}
 
-    /* Multiply each digit of num1 with each digit of num2 */
-    for (i = len1 - 1; i >= 0; i--)
-    {
-        if (num1[i] < '0' || num1[i] > '9')
-            return (NULL);
-        carry = 0;
-        for (j = len2 - 1; j >= 0; j--)
-        {
-            if (num2[j] < '0' || num2[j] > '9')
-                return (NULL);
-            sum = (num1[i] - '0') * (num2[j] - '0') + carry + (result[i + j + 1] - '0');
-            carry = sum / 10;
-            result[i + j + 1] = (sum % 10) + '0';
-        }
-        result[i + j + 1] = carry + '0';
-    }
+	/* Remove leading zeros from the result string */
+	for (i = 0; result[i] == '0'; i++);
+		if (i > 0)
+		{
+			for (j = 0; j < len1 + len2 - i + 1; j++)
+			{
+				result[j] = result[j + i];
+			}
+		}
+	else
+	{
+		j = len1 + len2;
 
-    /* Remove leading zeros from the result string */
-    for (i = 0; result[i] == '0'; i++);
-    if (i > 0)
-        for (j = 0; j < len1 + len2 - i + 1; j++)
-            result[j] = result[j + i];
-    else
-        j = len1 + len2;
+		/* Null-terminate the result string */
+		result[j] = '\0';
 
-    /* Null-terminate the result string */
-    result[j] = '\0';
-
-    return (result);
+		return (result);
+	}
 }
 
 /**
