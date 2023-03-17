fb6e0897b58180f86a2d593c490fb876a60ce038
Agunloye Oluwatobiloba <jgetitdonefast@gmail.com>
2023-03-17 03:12:55 +0100

    Update 101-mul.c
    We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,56 +3,89 @@
  * Author: TobiLight
 */
 
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 
 /**
- * _isdigit - checks if a character is a digit
- * @c: the character to check
- *
- * Return: 1 if c is a digit, 0 otherwise
-*/
-int _isdigit(int c)
-{
-	return (c >= '0' && c <= '9');
-}
-
-/**
  * main - multiplies two positive numbers
- * @argc: the number of arguments
- * @argv: the arguments
- *
- * Return: 0 on success, 98 on failure
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 on success, 98 on error
 */
-int main(int argc, char *argv[])
-{
-	unsigned int num1, num2, result;
-	int i, j;
 
+int main(int argc, char **argv)
+{
+	int i, j, k, carry, len1, len2, *result;
+	char *num1, *num2;
+
+	/* Check if number of arguments is correct */
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (98);
 	}
 
-	for (i = 1; i < 3; i++)
+	/* Check if num1 is composed of digits */
+	num1 = argv[1];
+	len1 = strlen(num1);
+	for (i = 0; i < len1; i++)
 	{
-		for (j = 0; argv[i][j]; j++)
+		if (!isdigit(num1[i]))
 		{
-			if (!_isdigit(argv[i][j]))
-			{
-				printf("Error\n");
-				return (98);
-			}
+		    printf("Error\n");
+		    return (98);
 		}
 	}
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
-	result = num1 * num2;
-	printf("%u\n", result);
+	/* Check if num2 is composed of digits */
+	num2 = argv[2];
+	len2 = strlen(num2);
+	for (i = 0; i < len2; i++)
+	{
+		if (!isdigit(num2[i]))
+		{
+		    printf("Error\n");
+		    return (98);
+		}
+	}
+
+	/* Allocate memory for result */
+	result = calloc(len1 + len2, sizeof(int));
+	if (result == NULL)
+	{
+		printf("Error\n");
+		return (98);
+	}
+
+	/* Multiply num1 and num2 digit by digit */
+	for (i = len1 - 1; i >= 0; i--)
+	{
+		carry = 0;
+		for (j = len2 - 1; j >= 0; j--)
+		{
+			k = i + j + 1;
+			result[k] += (num1[i] - '0') * (num2[j] - '0') + carry;
+			carry = result[k] / 10;
+			result[k] %= 10;
+		}
+		result[i] += carry;
+	}
+
+	/* Print result */
+	i = 0;
+	while (result[i] == 0 && i < len1 + len2 - 1)
+	{
+		i++;
+	}
+	while (i < len1 + len2)
+	{
+		putchar(result[i++] + '0');
+		putchar('\n');
+	}
+
+	/* Free memory */
+	free(result);
 	return (0);
 }
