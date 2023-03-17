bb20abd66101f8edb6e2e582a7f4934927a9667c
Agunloye Oluwatobiloba <jgetitdonefast@gmail.com>
2023-03-17 02:47:31 +0100

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -9,133 +9,50 @@
 #include <string.h>
 #include <ctype.h>
 
-void error(void);
-void print_result(char *result, int len);
-char *multiply(char *num1, char *num2);
-
 /**
- * error - Prints error then exit
+ * _isdigit - checks if a character is a digit
+ * @c: the character to check
  *
- * Return: exit(98)
+ * Return: 1 if c is a digit, 0 otherwise
 */
-
-void error(void)
+int _isdigit(int c)
 {
-	printf("Error\n");
-	exit(98);
+	return (c >= '0' && c <= '9');
 }
 
 /**
- * print_result - prints result
- * @result: multiplication result
- * @len: length of result
+ * main - multiplies two positive numbers
+ * @argc: the number of arguments
+ * @argv: the arguments
  *
- * Return: nothing
+ * Return: 0 on success, 98 on failure
 */
-
-void print_result(char *result, int len)
-{
-	int i;
-
-	for (i = 0; i < len; i++)
-	{
-		printf("%c", result[i]);
-	}
-	printf("\n");
-}
-
-/**
- * multiply - multiplies two positive numbers
- * @num1: positive number for multiplication
- * @num2: positive number for multiplication
- *
- * Return: nothing
-*/
-
-char *multiply(char *num1, char *num2)
-{
-	int len1 = 0, len2 = 0, i, j, carry, sum;
-	char *result;
-
-	/* Compute the length of the input strings */
-	while (num1[len1])
-		len1++;
-	while (num2[len2])
-		len2++;
-
-	/* Allocate space for the result string */
-	result = malloc(len1 + len2 + 1);
-	if (!result)
-		return (NULL);
-
-	/* Initialize the result string to zero */
-	for (i = 0; i < len1 + len2; i++)
-	{
-		result[i] = '0';
-		result[i] = '\0';
-	}
-	
-	/* Multiply each digit of num1 with each digit of num2 */
-	for (i = len1 - 1; i >= 0; i--)
-	{
-		if (num1[i] < '0' || num1[i] > '9')
-		    return (NULL);
-		carry = 0;
-		for (j = len2 - 1; j >= 0; j--)
-		{
-			if (num2[j] < '0' || num2[j] > '9')
-				return (NULL);
-			sum = (num1[i] - '0') * (num2[j] - '0') + carry + (result[i + j + 1] - '0');
-			carry = sum / 10;
-			result[i + j + 1] = (sum % 10) + '0';
-		}
-		result[i + j + 1] = carry + '0';
-	}
-
-	/* Remove leading zeros from the result string */
-	for (i = 0; result[i] == '0'; i++);
-		if (i > 0)
-		{
-			for (j = 0; j < len1 + len2 - i + 1; j++)
-			{
-				result[j] = result[j + i];
-			}
-		}
-	else
-	{
-		j = len1 + len2;
-
-		/* Null-terminate the result string */
-		result[j] = '\0';
-
-		return (result);
-	}
-}
-
-/**
- * main - Entry point
- * @argc: Argument count
- * @argv: Array of arguments
- *
- * Return: Always(0)
-*/
-
 int main(int argc, char *argv[])
 {
-	int i, j, len;
-
+	unsigned int num1, num2, result;
+	int i, j;
+	
 	if (argc != 3)
-		error();
-
-	for (i = 1; i < argc; i++)
 	{
-		len = strlen(argv[i]);
-		for (j = 0; j < len; j++)
+		printf("Error\n");
+		return (98);
+	}
+
+	for (i = 1; i < 3; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
 		{
-			if (!isdigit(argv[i][j]))
-				error();
+			if (!_isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (98);
+			}
 		}
 	}
-	multiply(argv[1], argv[2]);
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = num1 * num2;
+	printf("%u\n", result);
 	return (0);
 }
