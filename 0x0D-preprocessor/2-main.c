3e08558909e992cac5c71a5e183d2e2792f9ec0f
TobiLight <jgetitdonefast@gmail.com>
2023-03-20 20:08:44 -0700

    File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,18 @@
+/*
+ * File: 2-main.c
+ * Author: TobiLight
+*/
+
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	printf("%s", __FILE__);
+	return (0);
+}
