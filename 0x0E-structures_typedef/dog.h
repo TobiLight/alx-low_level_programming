8a0c933778f0c4cd2c5a190db7b5a4f2d0264a60
TobiLight <jgetitdonefast@gmail.com>
2023-03-20 20:42:05 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog's basic info
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ *
+ * Description: No description for this struct
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
