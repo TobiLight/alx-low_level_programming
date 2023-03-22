ae8cb8386d6b472c982c13d0630cbbd3f15a1a05
TobiLight <jgetitdonefast@gmail.com>
2023-03-22 07:36:47 -0700

    fixed betty errors

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -6,16 +6,17 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator - executes a function given 
- *                  as a parameter on each element of an array
+ * array_iterator - executes a function given
+ *                  as a parameter on each element
+ *                  of an array.
  * @array: Pointer to array of integers
  * @size: Size of the array
  * @action: Pointer to function to execute.
- * 
+ *
  * Return: nothing
 */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-    print("hello");
+	printf("hello");
 }
