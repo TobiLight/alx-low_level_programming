9c2ba205b82bd228094053dc7858e1b6136944c5
TobiLight <jgetitdonefast@gmail.com>
2023-03-22 07:12:08 -0700

    What's my name?

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,17 +1,17 @@
 /*
  * File: 0-print_name.c
  * Author: TobiLight
- */
+*/
 
 #include "function_pointers.h"
 
 /**
- * print_name -Prints a name
+ * print_name - Prints a name
  * @name: Pointer to name character to be printed
  * @f: Pointer to the print function
- */
+*/
 
 void print_name(char *name, void (*f)(char *))
 {
-    f(name);
+	f(name);
 }
