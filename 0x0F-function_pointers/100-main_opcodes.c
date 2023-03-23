643360eb6b152b2f7ff639ee1b2a93da33a9271b
TobiLight <jgetitdonefast@gmail.com>
2023-03-23 01:57:00 +0100

    Removed trailing whitespace

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -16,34 +16,34 @@
 
 int main(int argc, char *argv[])
 {
-    int bytes, i;
-    char *array;
+	int bytes, i;
+	char *array;
 
-    if (argc < 2)
-    {
-        printf("Error\n");
-        exit(1);
-    }
+	if (argc < 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
 
-    bytes = atoi(argv[1]);
+	bytes = atoi(argv[1]);
 
-    if (bytes < 0)
-    {
-        printf("Error\n"),
-        exit(2);
-    }
+	if (bytes < 0)
+	{
+		printf("Error\n"),
+		exit(2);
+	}
 
-    array = (char *)main;
+	array = (char *)main;
 
-    for (i = 0; i < bytes; i++)
-    {
-        if (i == bytes - 1)
-        {
-            printf("%02hhx\n", array[i]);
+	for (i = 0; i < bytes; i++)
+	{
+		if (i == bytes - 1)
+		{
+			printf("%02hhx\n", array[i]);
 			break;
-        }
-        printf("%02hhx ", array[i]);
-    }
+		}
+		printf("%02hhx ", array[i]);
+	}
 
-    return (0);
+	return (0);
 }
