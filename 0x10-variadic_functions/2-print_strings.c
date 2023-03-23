de1d7d676c1c17cdf5354146be79f301e33823dd
TobiLight <jgetitdonefast@gmail.com>
2023-03-23 14:42:35 +0100

    Updated code

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -29,13 +29,12 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		str = va_arg(args, char *);
 
 		if (str == NULL)
-			printf("(nil)");
-;
+			str = "(nil)";
+
+		printf("%s", str);
 		/* Remove separator at the end of last digit */
 		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
-
-		printf("%s%s", str, separator);
 	}
 
 	va_end(args);
