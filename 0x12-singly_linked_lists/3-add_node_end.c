7a6fd2bbb898b09830089740dc8c656cdc10a203
TobiLight <jgetitdonefast@gmail.com>
2023-03-30 23:24:24 +0000

    Function that adds a new node to the end of a list_t list.[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[Dadd the end of a list_t list.[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[Dt[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~ the end of a list_t list.[3[C[[C[C[C[C[C[C[C[C[C[C[C[C[C[C.

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -16,7 +16,7 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node;
-	list_t *last = *head;
+	list_t *last;
 
 	if (head == NULL || str == NULL)
 		return (NULL);
@@ -47,11 +47,10 @@ list_t *add_node_end(list_t **head, const char *str)
 		return (new_node);
 	}
 
+	last = *head;
 	/* If the node is not empty, traverse till the end */
 	while (last->next != NULL)
-	{
 		last = last->next;
-	}
 	last->next = new_node;
 	return (new_node);
 }
