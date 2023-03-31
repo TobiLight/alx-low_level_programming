9b52086e683c4b871039c73fa1bc46170cb06315
TobiLight <jgetitdonefast@gmail.com>
2023-03-31 00:02:00 +0000

    Function that executes before main()

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,23 +1,18 @@
 /*
  * File: 100-first.c
  * Author: TobiLight
-*/
+ */
 
 #include "lists.h"
 
 /**
- * before_main - A constructor function that prints a message before main is called
+ * before_main - A constructor function that prints a
+ *               message before main is called
  *
- * This function is executed automatically before the main function is called, thanks to the
- * GCC-specific __attribute__ ((constructor)) syntax. It simply prints the following message:
- *
- *     You're beat! and yet, you must allow,
- *     I bore my house upon my back!
- *
- * Return: This function does not return a value.
+ * Return: nothing
  */
 
-void __attribute__ ((constructor)) before_main()
+void my_constructor(void)
 {
-    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
