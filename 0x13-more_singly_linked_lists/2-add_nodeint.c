029d3d482d34e888a0706572e650ec7ada8d9bf3
TobiLight <jgetitdonefast@gmail.com>
2023-04-03 12:02:36 -0700

    function that adds a new node at the beginning of a listint_t list

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -7,8 +7,8 @@
 
 /**
  * add_nodeint - adds a new node at the beginning of a listint_t list.
- * @h - Pointer to a pointer to a node
- * @n - value of node data
+ * @head: Pointer to a pointer to a listint_t node.
+ * @n: value of node data
  *
  * Return: The address of the new element, or NULL if it failed
 */
