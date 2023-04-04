64b704380eb19ec7c3990855ed71a85ea7c0fefa
TobiLight <jgetitdonefast@gmail.com>
2023-04-03 20:00:19 -0700

    Fixed line over 80 characters

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -6,7 +6,8 @@
 #include "lists.h"
 
 /**
- * sum_listint - returns the sum of all the data (n) of a listint_t linked list.
+ * sum_listint - Returns the sum of all the data (n) of
+ *               a listint_t linked list.
  * @head: Pointer to a listint_t list
  *
  * Return: Sum of all data (n), otherwise if the list is empty, return 0
