3f76489cd404ae65400a2bdb03712f2e345e95cb
TobiLight <jgetitdonefast@gmail.com>
2023-04-03 21:58:28 -0700

    Function that inserts a new node at a given position.

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -40,6 +40,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	/* Assign the current node to current */
 	current = *head;
+
 	/* Loop through the list */
 	while (current && idx > 1)
 	{
