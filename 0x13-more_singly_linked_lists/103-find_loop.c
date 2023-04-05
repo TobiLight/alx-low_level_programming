a2a244000716ceb8dd963488504b1e55a7c622c5
TobiLight <jgetitdonefast@gmail.com>
2023-04-05 03:01:40 +0000

    function that finds the loop in a linked list.

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,49 @@
+/*
+ * File: 103-find_loop.c
+ * Author: TobiLight
+ */
+
+#include "lists.h"
+
+/**
+ * find_listint_loop - finds the loop in a linked list.
+ * @head: Pointer to a listint_t list
+ *
+ * Return: The address of the node where the loop starts
+ *         or NULL if there is no loop
+ */
+
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *slow, *fast;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+
+	slow = head->next;
+	fast = (head->next)->next;
+	while (fast)
+	{
+		if (slow == fast)
+		{
+			slow = head;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			slow = slow->next;
+			while (slow != fast)
+			{
+				slow = slow->next;
+			}
+			return ((void *)fast);
+		}
+		slow = slow->next;
+		fast = (fast->next)->next;
+	}
+
+	return (NULL);
+}
