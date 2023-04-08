3264dfcf6641f8ec70d4777b985243301390b4e5
TobiLight <jgetitdonefast@gmail.com>
2023-04-08 01:39:46 -0700

    Function that converts a binary number to an unsigned int.

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,15 @@
+/*
+ * File : main.h
+ * Author: TobiLight
+ * Description: Contains function prototypes, structs and/or
+ *              typedefs.
+*/
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+unsigned int binary_to_uint(const char *b);
+
+#endif /* MAIN_H */
