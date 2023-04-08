e645a54f28f366775b05f5d01fc81cf0072a1352
TobiLight <jgetitdonefast@gmail.com>
2023-04-08 10:52:17 +0000

    Function that prints the binary representation of a number.

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,41 @@
+/*
+ * File: 1-print_binary.c
+ * Author: TobiLight
+ */
+
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number.
+ * @n: number to be represented in binary
+ *
+ * Return: nothing
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int m;
+
+	/* If n is 0, print 0 and return */
+	if (n == 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	m = 1ul << (sizeof(unsigned long int) * 8 - 1);
+
+	/* Loop through each bit*/
+	while (m != 0)
+	{
+		/* If current bit is 1 */
+		if ((n & m) >> 1)
+			printf("1");
+		else
+			printf("0");
+
+		/* Move to the next bit */
+		m >>= 1;
+	}
+	printf("\n");
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -10,6 +10,8 @@
 
 #include <stdio.h>
 
+int _putchar(char);
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
 
 #endif /* MAIN_H */
