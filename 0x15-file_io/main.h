87f17c195e7f6a0c0fdf4b8c3510cc36f93128ca
TobiLight <jgetitdonefast@gmail.com>
2023-04-11 03:52:52 +0000

    Updated code

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,8 +8,8 @@
  *              typedefs.
  */
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <fcntl.h>
@@ -17,11 +17,13 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+
 void check_elf(unsigned char *e_ident);
 void print_magic(unsigned char *e_ident);
 void print_class(unsigned char *e_ident);
 void print_data(unsigned char *e_ident);
 void print_version(unsigned char *e_ident);
+
 void print_abi(unsigned char *e_ident);
 void print_osabi(unsigned char *e_ident);
 void print_type(unsigned int e_type, unsigned char *e_ident);
