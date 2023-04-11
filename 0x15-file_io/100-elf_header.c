1cb89ddfb7333f08664849eb6c9358cb907dbc71
TobiLight <jgetitdonefast@gmail.com>
2023-04-11 02:49:48 +0000

    Updated code

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -11,6 +11,13 @@
 #include <sys/types.h>
 #include <fcntl.h>
 
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+
 /**
  * print_abi - Prints the ABI version of an ELF header.
  * @e_ident: A pointer to an array containing the ELF ABI version.
