c6bc46b4e109dc6423d205becc49a33f1c853074
TobiLight <jgetitdonefast@gmail.com>
2023-04-11 01:46:58 +0000

    Updated code

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -29,7 +29,7 @@ int create_file(const char *filename, char *text_content)
 		len++;
 	}
 	wr = write(op, text_content, len);
-	op = open(filename, O_WRONLY | O_APPEND);
+	op = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (op == -1 || wr == -1)
 		return (-1);
