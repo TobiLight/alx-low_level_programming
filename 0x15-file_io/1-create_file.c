14a61a8572f9ffd031c8eb1cfd8469fd1ed8cfcf
TobiLight <jgetitdonefast@gmail.com>
2023-04-11 01:46:23 +0000

    Function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,14 +19,16 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	if (text_content != NULL)
+
+	if (text_content == NULL)
 	{
-		while (text_content[len])
-		{
-			len++;
-		}
-		wr = write(op, text_content, len);
+		text_content = "";
 	}
+	while (text_content[len])
+	{
+		len++;
+	}
+	wr = write(op, text_content, len);
 	op = open(filename, O_WRONLY | O_APPEND);
 
 	if (op == -1 || wr == -1)
