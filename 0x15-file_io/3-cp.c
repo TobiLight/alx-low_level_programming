2a2983791544692fc244144e678acbbf9f48467b
TobiLight <jgetitdonefast@gmail.com>
2023-05-08 16:44:53 -0700

    a program that copies the content of a file to another file.

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,97 @@
+/*
+ * File: 3-cp.c
+ * Auth: TobiLight
+ */
+
+#include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+char *create:_buffer(char *file);
+void close_fd(int fd);
+
+/**
+ * create_buffer - Allocates memory (1024) bytes for a buffer.
+ * @file: The name of the file buffer is storing chars for.
+ *
+ * Return: A pointer to the newly-allocated buffer.
+ */
+
+char *create_buffer(char *file)
+{
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * 1024);
+	if (buffer == NULL)
+	{
+		dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", file);
+		exit(99);
+	}
+	return (buffer);
+}
+
+/**
+ * close_fd - Closes file descriptors.
+ * @fd: file descriptor to be closed.
+ */
+void close_fd(int fd)
+{
+	int f;
+
+	f = close(fd);
+	if (f == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
+ * main - Copies the contents of a file to another file.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: 0 (Always)
+ */
+int main(int argc, char *argv[])
+{
+	int file_from, file_to, rd, wr;
+	char *buffer;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	buffer = create_buffer(argv[2]);
+	file_from = open(argv[1], O_RDONLY);
+	rd = read(file_from, buffer, 1024);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	do {
+		if (file_from == -1 || rd == -1)
+		{
+			dprintf(STDERR_FILENO,
+					"Error: Can't read from file %s\n", argv[1]);
+			free(buffer);
+			exit(98);
+		}
+
+		wr = write(file_to, buffer, rd);
+		if (file_to == -1 || wr == -1)
+		{
+			dprintf(STDERR_FILENO,
+					"Error: Can't write to %s\n", argv[2]);
+			free(buffer);
+			exit(99);
+		}
+
+		rd = read(file_from, buffer, 1024);
+		file_to = open(argv[2], O_WRONLY | O_APPEND);
+	} while (rd > 0);
+	free(buffer);
+	close_fd(file_from);
+	close_fd(file_to);
+	return (0);
+}
