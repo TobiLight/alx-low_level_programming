daf0c1c377bd5928a3db1027cd8e3dcd8260a5a2
TobiLight <jgetitdonefast@gmail.com>
2023-06-16 23:39:54 +0100

    Function that inserts a new node in a dlistint_t list at a given position.

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -23,13 +23,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-	if (*h == NULL)
-		return (NULL);
 	for (i = 0; i != idx; i++)
 	{
 		if (i == idx)
 			break;
- 		curr = curr->next;
+		curr = curr->next;
 		if (curr == NULL)
 			return (NULL);
 	}
