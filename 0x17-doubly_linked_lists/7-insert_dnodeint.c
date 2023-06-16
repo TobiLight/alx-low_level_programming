b80317394b1dd950e062c43a952a419e566e53f7
TobiLight <jgetitdonefast@gmail.com>
2023-06-16 22:14:19 +0100

    Function that inserts a new node in a distint_t list at a given position.

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -41,7 +41,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	new_node->n = n;
 	new_node->prev = temp;
-	new_node->next = curr;
+	new_node->next = temp->next;
 	temp->next = new_node;
 	return (new_node);
 }
