2873dfe5e266f0e43e867b59a3490c34c2bd0f66
TobiLight <jgetitdonefast@gmail.com>
2023-06-16 18:04:26 +0100

    Function that frees a linked dlistint_t list.

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -8,13 +8,12 @@
 /**
  * free_dlistint - Frees a linked dlistint_t list.
  * @head: The head of the dlistint_t list.
-*/
+ */
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *temp, *curr = head;
 
-	
-	while(curr != NULL)
+	while (curr != NULL)
 	{
 		temp = curr;
 		curr = curr->next;
