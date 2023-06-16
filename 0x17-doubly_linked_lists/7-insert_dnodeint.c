344e83180a7bc1d090ed91e96f6e4e52eca5c8f1
TobiLight <jgetitdonefast@gmail.com>
2023-06-17 00:18:37 +0100

    Function that inserts a new node in a dlistint_t list at a give position.

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -18,43 +18,28 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *curr = *h, *new_node;
-	size_t len;
 	unsigned int i = 0;
 
 	if (idx == 0)
 		return (add_dnodeint(h, n));
-	for (len = 0; curr != NULL; len++)
+	for (i = 0; i < idx - 1; i++)
 	{
+		if (curr == NULL)
+			return (NULL);
 		curr = curr->next;
 	}
 
-	if (idx == len - 1)
+	if (curr == NULL || curr->next == NULL)
 		return (add_dnodeint_end(h, n));
+
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
+
 	new_node->n = n;
-	if (*h == NULL)
-	{
-		new_node->prev = NULL;
-		new_node->next = NULL;
-		*h = new_node;
-		return (new_node);
-	}
-	curr = *h;
-	while (curr)
-	{
-		if (i == idx)
-		{
-			new_node->next = curr;
-			new_node->prev = curr->prev;
-			curr->prev->next = new_node;
-			curr->prev = new_node;
-			return (new_node);
-		}
-		curr = curr->next;
-		i++;
-	}
-	free(new_node);
-	return (NULL);
+	new_node->prev = curr;
+	new_node->next = curr->next;
+	curr->next->prev = new_node;
+	curr->next = new_node;
+	return (new_node);
 }
