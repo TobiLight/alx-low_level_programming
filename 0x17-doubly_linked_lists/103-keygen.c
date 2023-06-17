2372bdba0c27229bbd656c3d156f497ce343302c
TobiLight <jgetitdonefast@gmail.com>
2023-06-17 03:46:29 +0100

    Keygen for crackme5

---
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -0,0 +1,72 @@
+/*
+ * File: 103-keygen.c
+ * Author: Oluwatobiloba Light
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - Generates and prints passwords for
+ *        the crackme5 executable.
+ * @argc: number of arguments supplied to the program.
+ * @argv: array of pointers to the arguments.
+ *
+ * Return: Always 0.
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+    char password[7], *codex;
+    int length = strlength(argv[1]), i, temp;
+
+    codex = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+
+    temp = (length ^ 59) & 63;
+    password[0] = codex[temp];
+
+    temp = 0;
+    i = 0;
+    while (i < length)
+    {
+        temp += argv[1][i];
+        i++;
+    }
+    password[1] = codex[(temp ^ 79) & 63];
+    temp = 1;
+    i = 0;
+    while (i < length)
+    {
+        temp *= argv[1][i];
+        i++;
+    }
+    password[2] = codex[(temp ^ 85) & 63];
+    temp = 0;
+    i = 0;
+    while (i < length)
+    {
+        if (argv[1][i] > temp)
+            temp = argv[1][i];
+        i++;
+    }
+    srand(temp ^ 14);
+    password[3] = codex[rand() & 63];
+    temp = 0;
+    i = 0;
+    while (i < length)
+    {
+        temp += (argv[1][i] * argv[1][i]);
+        i++;
+    }
+    password[4] = codex[(temp ^ 239) & 63];
+    i = 0;
+    while (i < argv[1][0])
+    {
+        temp = rand();
+        i++;
+    }
+    password[5] = codex[(temp ^ 229) & 63];
+    password[6] = '\0';
+    printf("%s", password);
+    return (0);
+}
