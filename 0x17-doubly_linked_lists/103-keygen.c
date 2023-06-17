57d3476290738dbcfc53d85da270e72948311920
TobiLight <jgetitdonefast@gmail.com>
2023-06-17 03:49:24 +0100

    Keygen for crackme5

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -17,57 +17,57 @@
  */
 int main(int argc, char *argv[])
 {
-    (void) argc;
-    char password[7], *codex;
-    int length = strlen(argv[1]), i, temp;
+	(void)argc;
+	char password[7], *codex;
+	int length = strlen(argv[1]), i, temp;
 
-    codex = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+	codex = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
 
-    temp = (length ^ 59) & 63;
-    password[0] = codex[temp];
+	temp = (length ^ 59) & 63;
+	password[0] = codex[temp];
 
-    temp = 0;
-    i = 0;
-    while (i < length)
-    {
-        temp += argv[1][i];
-        i++;
-    }
-    password[1] = codex[(temp ^ 79) & 63];
-    temp = 1;
-    i = 0;
-    while (i < length)
-    {
-        temp *= argv[1][i];
-        i++;
-    }
-    password[2] = codex[(temp ^ 85) & 63];
-    temp = 0;
-    i = 0;
-    while (i < length)
-    {
-        if (argv[1][i] > temp)
-            temp = argv[1][i];
-        i++;
-    }
-    srand(temp ^ 14);
-    password[3] = codex[rand() & 63];
-    temp = 0;
-    i = 0;
-    while (i < length)
-    {
-        temp += (argv[1][i] * argv[1][i]);
-        i++;
-    }
-    password[4] = codex[(temp ^ 239) & 63];
-    i = 0;
-    while (i < argv[1][0])
-    {
-        temp = rand();
-        i++;
-    }
-    password[5] = codex[(temp ^ 229) & 63];
-    password[6] = '\0';
-    printf("%s", password);
-    return (0);
+	temp = 0;
+	i = 0;
+	while (i < length)
+	{
+		temp += argv[1][i];
+		i++;
+	}
+	password[1] = codex[(temp ^ 79) & 63];
+	temp = 1;
+	i = 0;
+	while (i < length)
+	{
+		temp *= argv[1][i];
+		i++;
+	}
+	password[2] = codex[(temp ^ 85) & 63];
+	temp = 0;
+	i = 0;
+	while (i < length)
+	{
+		if (argv[1][i] > temp)
+			temp = argv[1][i];
+		i++;
+	}
+	srand(temp ^ 14);
+	password[3] = codex[rand() & 63];
+	temp = 0;
+	i = 0;
+	while (i < length)
+	{
+		temp += (argv[1][i] * argv[1][i]);
+		i++;
+	}
+	password[4] = codex[(temp ^ 239) & 63];
+	i = 0;
+	while (i < argv[1][0])
+	{
+		temp = rand();
+		i++;
+	}
+	password[5] = codex[(temp ^ 229) & 63];
+	password[6] = '\0';
+	printf("%s", password);
+	return (0);
 }
