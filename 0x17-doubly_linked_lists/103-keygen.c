60047af8c8f18b1e4c8328b99743073b5199d8f1
TobiLight <jgetitdonefast@gmail.com>
2023-06-17 03:48:32 +0100

    Keygen for crackme5

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -15,10 +15,11 @@
  *
  * Return: Always 0.
  */
-int main(int __attribute__((__unused__)) argc, char *argv[])
+int main(int argc, char *argv[])
 {
+    (void) argc;
     char password[7], *codex;
-    int length = strlength(argv[1]), i, temp;
+    int length = strlen(argv[1]), i, temp;
 
     codex = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
 
