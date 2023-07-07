13ef5f6e99f9fdaa3e56212b9cd27e9460c42ee3
TobiLight <jgetitdonefast@gmail.com>
2023-07-08 00:31:03 +0100

    Function that creates a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -19,7 +19,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table = malloc(sizeof(hash_table_t));
 	if (hash_table == NULL)
 		return (NULL);
-	hash_table->array = malloc(sizeof(hash_table_t) * size);
+	hash_table->array = malloc(sizeof(hash_table_t *) * size);
 	if (hash_table->array == NULL)
 	{
 		free(hash_table);
