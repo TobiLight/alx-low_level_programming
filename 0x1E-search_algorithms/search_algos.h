9e8a57713095c61e8ee5d27aa72fee3d03ac74d6
TobiLight <jgetitdonefast@gmail.com>
2023-10-10 20:32:50 +0100

    Added header file for project

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,14 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+/**
+ * File: search_algos.h
+ * Author: Oluwatobiloba Light
+ * Description: Contains all the function prototypes for this project.
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int linear_search(int *array, size_t size, int value);
+
+#endif /* SEARCH_ALGOS_H */
