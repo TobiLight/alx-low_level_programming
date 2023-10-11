14d965a49d2c0e71f9229d312699f92c0ea486f0
TobiLight <jgetitdonefast@gmail.com>
2023-10-11 15:36:34 +0100

    Searches for a value in a sorted array of integers using the Interpolation search algorithm

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -40,6 +40,5 @@ int interpolation_search(int *array, size_t size, int value)
 	pos = low + (((double)(high - low) / (array[high] - array[low])) *
 				 (value - array[low]));
 	printf("Value checked array[%ld] is out of range\n", pos);
-
 	return (-1);
 }
